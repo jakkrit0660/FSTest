0a8abe05298d3252c8e9772866a73798bd32cd14
jakrit0660 <jakrit0660@gmail.com>
2021-01-18 12:42:44 +0700

    Add myTest.vue

---
+++ src/components/myTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <body>
+    <h1>Hello World</h1>
+    Type Something: <input type="text" id="text" /><br />
+    <button @click="showtext()">Click</button><br />
+    <p>The text will show down here</p>
+    <p id="line1"></p>
+  </body>
+</template>
+<script>
+export default {
+  methods: {
+    showtext() {
+      var Stext = document.getElementById("text").value;
+      document.getElementById("line1").innerHTML = Stext;
+    },
+  },
+};
+</script>
